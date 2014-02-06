c9cb1876fec05b6ef85b1b7da737424f8a5524fb
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-02-06 22:58:28 +0100

    Added documentation to the crypto service

--- src/app/services/meCryptoSvc.js
+++ src/app/services/meCryptoSvc.js
@@ -6,7 +6,15 @@
     angular.module("mitme.services.crypto", ["mitme.services.objc"])
         .factory("crypto", function (objc) {
             return {
-                encrypt: function (object, key) {
+                /**
+                 * Encrypt the message for decryption later.
+                 * The user is needed to access all the device's keys.
+                 * 
+                 * @param {object} the object we want to encrypt
+                 * @param {User} user the user that we encrypt the message for
+                 * @return {string} an encrypted string, that can be passed to the decrypt() method
+                 */
+                encrypt: function (object, user) {
                     /* Implement me, native side
                     return objc.call("crypto", "encrypt", {
                         text: text,
@@ -16,6 +24,13 @@
                     
                     return JSON.stringify(object);
                 },
+                
+                /**
+                 * Attempts to decrypt the message with the key stored on this device, if any.
+                 * 
+                 * @param {string} blob the encrypted data
+                 * @return {object} the decrypted object, or undefined if we could not decrypt.
+                 */
                 decrypt: function (blob) {
                     /* TODO Implement me, native side
                     return objc.call("crypto", "decrypt", {
