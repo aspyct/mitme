13c3b1ad1638a97e7f69cee7e7760353775f5d43
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-02-06 23:28:47 +0100

    Added some documentation in the Auth service

--- src/app/services/meAuthSvc.js
+++ src/app/services/meAuthSvc.js
@@ -4,6 +4,9 @@
 (function () {
     "use strict";
     
+    /**
+     * @module mitme.services.auth
+     */
     angular.module("mitme.services.auth", ["mitme.services.firebase"])
         .factory("auth", function ($firebaseSimpleLogin, $timeout, mitmeRef) {
             var currentUser,
@@ -34,10 +37,18 @@
                 });
             });
             
+            /**
+             * Authentication manager
+             * 
+             * @class Auth
+             */
             return {
                 /**
                  * Is the user logged in?
+                 * 
                  * Three state logic: true, false or undefined.
+                 * 
+                 * @method loggedIn
                  * @return {Boolean} true if connected, false if not, undefined if we don't know
                  */
                 loggedIn: function () {
@@ -47,12 +58,36 @@
                         return undefined;
                     }
                 },
+                
+                /**
+                 * Get the current user
+                 * 
+                 * @return {AuthUser} the authenticated user object. Can be null or undefined depending on login state.
+                 */
                 currentUser: function () {
                     return currentUser;
                 },
+                
+                /**
+                 * Offers to the user to login
+                 * 
+                 * Current implementation offers login via facebook only.
+                 */
                 login: function () {
                     auth.login("facebook");
                 },
+                
+                /**
+                 * Register an authentication listener function.
+                 * 
+                 * The listener takes exactly one argument: the currentUser.
+                 * It will be called instantaneously, as well as each time
+                 * the login status changes (login, logout or error).
+                 * Refer to the documentation of currentUser() for more info
+                 * on the argument passed.
+                 * 
+                 * @param listener {Function} the listener function
+                 */
                 addAuthListener: function (listener) {
                     listeners.push(listener);
                     
@@ -60,6 +95,10 @@
                         listener(currentUser);
                     });
                 },
+                
+                /**
+                 * Remove a previously registered auth listener.
+                 */
                 removeAuthListener: function (listener) {
                     var position;
                     
