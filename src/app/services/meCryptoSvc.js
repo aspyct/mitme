732a9a43af448e40adbe48a24b7004538b86fb9e
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-02-06 23:57:25 +0100

    Added documentation for the crypto service

--- src/app/services/meCryptoSvc.js
+++ src/app/services/meCryptoSvc.js
@@ -3,13 +3,26 @@
 (function () {
     "use strict";
     
+    /**
+     * @module mitme.services.crypto
+     */
     angular.module("mitme.services.crypto", ["mitme.services.objc"])
         .factory("crypto", function (objc) {
+            /**
+             * # Cryptography manager
+             * 
+             * This class is responsible for encrypting and decrypting stuff.
+             * It also manages the public/private key pair for the current device.
+             * 
+             * @class Crypto
+             */
             return {
                 /**
                  * Encrypt the message for decryption later.
+                 * 
                  * The user is needed to access all the device's keys.
                  * 
+                 * @method encrypt
                  * @param {object} the object we want to encrypt
                  * @param {User} user the user that we encrypt the message for
                  * @return {string} an encrypted string, that can be passed to the decrypt() method
@@ -28,6 +41,7 @@
                 /**
                  * Attempts to decrypt the message with the key stored on this device, if any.
                  * 
+                 * @method decrypt
                  * @param {string} blob the encrypted data
                  * @return {object} the decrypted object, or undefined if we could not decrypt.
                  */
@@ -39,6 +53,18 @@
                     */
                     
                     return JSON.parse(blob);
+                },
+                
+                /**
+                 * Generates a new key pair for this device.
+                 * 
+                 * The previous key pair is lost.
+                 * 
+                 * @method generateKeyPair
+                 * @return {String} the new public key.
+                 */
+                generateKeyPair: function () {
+                    
                 }
             };
         });
