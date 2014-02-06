bc9d0e119f568fc895a5b0861042906ab8966302
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2014-02-06 23:11:38 +0100

    Added documentation for amitme.service.assert module

--- src/app/services/meAssertSvc.js
+++ src/app/services/meAssertSvc.js
@@ -3,6 +3,15 @@
 (function () {
     "use strict";
     
+    /**
+     * Provide various assertions.
+     * 
+     * All of these assertions throw errors when they fail.
+     * An assertion is used to denote a programmatic error, so these
+     * errors should be reported to a system somewhere.
+     * 
+     * @module mitme.services.assert
+     */
     angular.module("mitme.services.assert", [])
         .factory("assert", function () {
             function assert(test, message) {
@@ -11,14 +20,36 @@
                 }
             }
             
+            /**
+             * @class Assert
+             */
             return {
+                /**
+                 * Asserts that value is true.
+                 * 
+                 * @method isTrue
+                 * @param value {Boolean} the value that must be true
+                 * @param [message] {String} the failure message
+                 */
                 isTrue: function (value, message) {
                     if (message === undefined) {
-                        message = "Value must be true";
+                        message = "Value must be truthy";
                     }
                     
                     assert(value, message);
                 },
+                
+                /**
+                 * Asserts that an object has a certain attribute.
+                 * 
+                 * Does not do any assertions on the value of the attribute,
+                 * except that it must not be undefined.
+                 * 
+                 * @method hasAttr
+                 * @param object {Object} the object that must have an attribute
+                 * @param attr {String} the attribute name
+                 * @param [message] the failure message
+                 */
                 hasAttr: function (object, attr, message) {
                     if (message === undefined) {
                         message = "Attribute " + attr + " must exist on object";
@@ -26,6 +57,14 @@
                 
                     assert(object[attr] !== undefined, message);
                 },
+                
+                /**
+                 * Asserts that a variable is a String
+                 * 
+                 * @method isString
+                 * @param value {String} the value that must be a string
+                 * @param [message] {String} the failure message
+                 */
                 isString: function (value, message) {
                     if (message === undefined) {
                         message = "Value must be of type string";
@@ -33,6 +72,14 @@
                     
                     assert(typeof value === "string", message);
                 },
+                
+                /**
+                 * Asserts that a variable is a Number
+                 * 
+                 * @method isNumber
+                 * @param value {Number} the value that must be a number
+                 * @param [message] {String} the failure message
+                 */
                 isNumber: function (value, message) {
                     if (message === undefined) {
                         message = "Value must be of type number";
@@ -40,6 +87,16 @@
                     
                     assert(typeof value === "number", message);
                 },
+                
+                /**
+                 * Asserts that a variable is not null.
+                 * 
+                 * Value can still be undefined.
+                 * 
+                 * @method notNull
+                 * @param value {any} the value that must not be null
+                 * @param [message] {String} the failure message
+                 */
                 notNull: function (value, message) {
                     if (message === undefined) {
                         message = "Value must not be null";
